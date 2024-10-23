19352ec4a2dd2a59a7d4fe999880df1bce75979f
official.srv.modak1@gmail.com <72427070+official-srv-modak@users.noreply.github.com>
2024-10-22 23:07:17 -0400

    added movies to the server

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -10,26 +10,32 @@ const Movie = ({ title }) => {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzYyNzI1N2VhNDM5N2IyM2EzN2FhYTI0ZGMyNWY3OCIsIm5iZiI6MTcyOTQ2NzUwNC41Njk3NDksInN1YiI6IjY3MTU5Mzc1OTk0MzYzN2NlNTgyODBjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iGJHIOXTFRvQuNcaTxl26ZcI4XM6ZU72f5ueXjCMRdY'
+            Authorization: 'Bearer your_token_here' // Replace with your actual token
         }
     };
 
     const handleWheel = (event) => {
         event.preventDefault();
-        cardsRef.current.scrollLeft += event.deltaY;
+        if (cardsRef.current) {
+            cardsRef.current.scrollLeft += event.deltaY;
+        }
     }
 
     useEffect(() => {
         fetch(`http://localhost/get_movies_list_json.php`, options)
             .then(response => response.json())
-            .then(response => setApiData(response.cards)) // Accessing 'cards' instead of 'results'
+            .then(response => setApiData(response.cards))
             .catch(err => console.error(err));
 
-        cardsRef.current.addEventListener('wheel', handleWheel);
-        
+        if (cardsRef.current) {
+            cardsRef.current.addEventListener('wheel', handleWheel);
+        }
+
         return () => {
             // Cleanup event listener on unmount
-            cardsRef.current.removeEventListener('wheel', handleWheel);
+            if (cardsRef.current) {
+                cardsRef.current.removeEventListener('wheel', handleWheel);
+            }
         };
     }, []);
 
@@ -38,9 +44,9 @@ const Movie = ({ title }) => {
             <h2>{title ? title : "Movies"}</h2>
             <div className="movie-list" ref={cardsRef}>
                 {apiData.map((card) => (
-                    <Link to={`/player/${card.id}`} className="card" key={card.id}>
-                        <img src={card.album_art_path} alt={card.name} /> {/* Use album_art_path for the image */}
-                        <p>{card.name}</p> {/* Use name for the title */}
+                    <Link to={`/player/${card.id}`} className="card" key={card.id} state={{ url: card.url }}>
+                        <img src={card.album_art_path} alt={card.name} />
+                        <p>{card.name}</p>
                     </Link>
                 ))}
             </div>
